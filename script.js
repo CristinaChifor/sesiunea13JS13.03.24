182ae9185bcc9c70b24f406f84e06307c160bed4
Cristina.Chifor <cristinachifor93.cc@gmail.com>
2024-03-13 19:11:13 +0200

    Add examples with parameters functions

--- script.js
+++ script.js
@@ -5,8 +5,7 @@
 //Funcțiile pot fi executa operații în baza unor parametri (date de intrare) și pot returna (întoarce) o valoare în urma execuției (date de ieșire).
 
 // Functii simple
-// Sunt compuse din cuvantul cheie function urmat de numele funcției urmat de paranteze rotunde intre care definim parametrii funcției (param1, param2) urmate de paranteze acolade { } în interiorul cărora scriem liniile de cod ce dorim sa fie executate în momentul apelarii funcției.
-
+// Funcțiile simple sunt compuse din cuvantul cheie function urmat de numele funcției urmat de paranteze rotunde () urmate de paranteze acolade { } în interiorul cărora scriem liniile de cod ce dorim sa fie executate în momentul apelarii funcției.
 
 function printDigits() {
     for (let i = 0; i <= 9; i++) {
@@ -15,3 +14,38 @@ function printDigits() {
    
 }
 printDigits();
+//printDigits();
+//printDigits();
+
+// Funcții cu parametrii
+// Definirea funcțiilor cu parametrii:
+//Sunt compuse din cuvantul cheie function urmat de numele funcției urmat de paranteze rotunde intre care definim parametrii funcției (param1, param2) urmate de paranteze acolade { } în interiorul cărora scriem liniile de cod ce dorim sa fie executate în momentul apelarii funcției.
+
+// Apelarea funcțiilor cu parametri:
+// Se realizeaza prin folosirea numelui funcției unde dorim sa se execute blocul de cod urmat de parantezele rotunde intre care trecem valori reale pentru parametri - aceste valori se numesc argumente. 
+
+function arrayHasElement(arrayToCheck, element) {
+    let found = false;
+for (const elem of arrayToCheck) {
+    if (elem === element) {
+         found= true;
+        break;
+    }
+}
+if (found) {
+    console.log(`Array ${arrayToCheck} has element ${element}`);
+} else {
+    console.log(`Array ${arrayToCheck} does not element ${element}`);
+}
+
+}
+
+ const array1 = [1, 2, 3, 4, 5];
+ const array2 = [3, 10, 21];
+ const valueToCheck1 = 10;
+ const valueToCheck2 = 30;
+
+ arrayToCheck(array1, valueToCheck1);
+ arrayToCheck(array2, valueToCheck1);
+ arrayToCheck(array1, valueToCheck2);
+arrayToCheck(['ala', 'bala', 'portocala'], 'ala');
