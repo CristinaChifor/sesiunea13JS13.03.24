1dc46a7dbcf267012c5d8468257fa1dbedf57dc9
Cristina.Chifor <cristinachifor93.cc@gmail.com>
2024-03-13 19:25:02 +0200

    Add examples for functions with return

--- script.js
+++ script.js
@@ -24,28 +24,49 @@ printDigits();
 // Apelarea funcțiilor cu parametri:
 // Se realizeaza prin folosirea numelui funcției unde dorim sa se execute blocul de cod urmat de parantezele rotunde intre care trecem valori reale pentru parametri - aceste valori se numesc argumente. 
 
-function arrayHasElement(arrayToCheck, element) {
-    let found = false;
-for (const elem of arrayToCheck) {
-    if (elem === element) {
-         found= true;
-        break;
-    }
-}
-if (found) {
-    console.log(`Array ${arrayToCheck} has element ${element}`);
-} else {
-    console.log(`Array ${arrayToCheck} does not element ${element}`);
-}
+//function arrayHasElement(arrayToCheck, element) {
+    //let found = false;
+//for (const elem of arrayToCheck) {
+    //if (elem === element) {
+        // found= true;
+       // break;
+    //}
+//}
+//if (found) {
+    //console.log(`Array ${arrayToCheck} has element ${element}`);
+//} else {
+    //console.log(`Array ${arrayToCheck} does not element ${element}`);
+//}
 
-}
+//}
 
  const array1 = [1, 2, 3, 4, 5];
  const array2 = [3, 10, 21];
- const valueToCheck1 = 10;
- const valueToCheck2 = 30;
+ //const valueToCheck1 = 10;
+ //const valueToCheck2 = 30;
 
- arrayToCheck(array1, valueToCheck1);
- arrayToCheck(array2, valueToCheck1);
- arrayToCheck(array1, valueToCheck2);
-arrayToCheck(['ala', 'bala', 'portocala'], 'ala');
+ //arrayToCheck(array1, valueToCheck1);
+ //arrayToCheck(array2, valueToCheck1);
+ //arrayToCheck(array1, valueToCheck2);
+//arrayToCheck(['ala', 'bala', 'portocala'], 'ala');
+
+// Funcții cu return
+
+function removeEvenNumber(arrayToModify) {
+    const newArray = [];
+
+    for (const num of arrayToModify) {
+        if(num % 2 > 0) {
+            newArray.push(num);
+        }
+
+    }
+    return newArray;
+}
+
+const array1Modified = removeEvenNumber(array1);
+console.log(`modified array1 is:  ${array1Modified}`);
+
+const array2Modified = removeEvenNumber(array2);
+console.log(`modified array2 is:  ${array2Modified}`);
+
