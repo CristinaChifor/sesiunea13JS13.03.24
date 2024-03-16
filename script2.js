15903454aefbb83dda7bb59ffcd020956ee1bcc3
Cristina.Chifor <cristinachifor93.cc@gmail.com>
2024-03-16 13:43:00 +0200

    Add JS Simple functions examples

---
+++ script2.js
@@ -0,0 +1,17 @@
+console.log(`Hello`);
+
+
+// Funcțiile sunt un aspect fundamental în JavaScript și sunt folosite pentru structurarea, eficientizarea și organizarea codului.
+//Funcțiile pot fi executa operații în baza unor parametri (date de intrare) și pot returna (întoarce) o valoare în urma execuției (date de ieșire).
+
+// Functii simple Funcțiile simple sunt compuse din cuvantul cheie function urmat de numele funcției urmat de paranteze rotunde () urmate de paranteze acolade { } în interiorul cărora scriem liniile de cod ce dorim sa fie executate în momentul apelarii funcției.
+
+function printDigits(){
+    for (let i =0; i <= 9; i++) {
+        console.log(`digit: `, i);
+    }
+}
+
+printDigits();// apelam functia
+printDigits();
+printDigits();
